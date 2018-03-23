b9bdda265129f4c2d9b953140d11e80a98d5e3e2
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2018-03-23 20:47:27 +0000

    use tabs

--- c_count.c
+++ c_count.c
@@ -28,8 +28,8 @@
 
 /* A custom structure to hold separate file and directory counts */
 struct filecount {
-  long dirs;
-  long files;
+	long dirs;
+	long files;
 };
 
 /*
@@ -39,52 +39,53 @@ struct filecount {
  * counts - pointer to struct containing file/dir counts
  */
 void count(char *path, struct filecount *counts) {
-    DIR *dir;                /* dir structure we are reading */
-    struct dirent *ent;      /* directory entry currently being processed */
-    char subpath[PATH_MAX];  /* buffer for building complete subdir and file names */
-    /* Some systems don't have dirent.d_type field; we'll have to use stat() instead */
+	DIR *dir;                /* dir structure we are reading */
+	struct dirent *ent;      /* directory entry currently being processed */
+	char subpath[PATH_MAX];  /* buffer for building complete subdir and file names */
+	/* Some systems don't have dirent.d_type field; we'll have to use stat() instead */
 #if !defined ( _DIRENT_HAVE_D_TYPE )
-    struct stat statbuf;     /* buffer for stat() info */
+	struct stat statbuf;     /* buffer for stat() info */
 #endif
 
-    dir = opendir(path);
+	dir = opendir(path);
 
-    /* opendir failed... file likely doesn't exist or isn't a directory */
-    if(NULL == dir) {
-        perror(path);
-        return;
-    }
+	/* opendir failed... file likely doesn't exist or isn't a directory */
+	if(NULL == dir) {
+		perror(path);
+		return;
+	}
 
-    while((ent = readdir(dir))) {
-      if (strlen(path) + 1 + strlen(ent->d_name) > PATH_MAX) {
-          fprintf(stdout, "path too long (%ld) %s%c%s", (strlen(path) + 1 + strlen(ent->d_name)), path, PATH_SEPARATOR, ent->d_name);
-          return;
-      }
+	while((ent = readdir(dir))) {
+		if (strlen(path) + 1 + strlen(ent->d_name) > PATH_MAX) {
+			fprintf(stdout, "path too long (%ld) %s%c%s", (strlen(path) + 1 + strlen(ent->d_name)), path, PATH_SEPARATOR, ent->d_name);
+			return;
+		}
 
-/* Use dirent.d_type if present, otherwise use stat() */
+		/* Use dirent.d_type if present, otherwise use stat() */
 #if ( defined ( _DIRENT_HAVE_D_TYPE ))
-      if(DT_DIR == ent->d_type) {
+		if(DT_DIR == ent->d_type) {
 #else
-      sprintf(subpath, "%s%c%s", path, PATH_SEPARATOR, ent->d_name);
-      if(lstat(subpath, &statbuf)) {
-          perror(subpath);
-          return;
-      }
+		sprintf(subpath, "%s%c%s", path, PATH_SEPARATOR, ent->d_name);
+		if(lstat(subpath, &statbuf)) {
+			perror(subpath);
+			return;
+		}
 
-      if(S_ISDIR(statbuf.st_mode)) {
+		if(S_ISDIR(statbuf.st_mode)) {
 #endif
-          /* Skip "." and ".." directory entries... they are not "real" directories */
-          if(0 == strcmp("..", ent->d_name) || 0 == strcmp(".", ent->d_name)) {
-/*              fprintf(stderr, "This is %s, skipping\n", ent->d_name); */
-          } else {
-              sprintf(subpath, "%s%c%s", path, PATH_SEPARATOR, ent->d_name);
-              counts->dirs++;
-              count(subpath, counts);
-          }
-      } else {
-          counts->files++;
-      }
-    }
+			/* Skip "." and ".." directory entries... they are not 
+			 * "real" directories */
+			if(0 == strcmp("..", ent->d_name) || 0 == strcmp(".", ent->d_name)) {
+				/*              fprintf(stderr, "This is %s, skipping\n", ent->d_name); */
+			} else {
+				sprintf(subpath, "%s%c%s", path, PATH_SEPARATOR, ent->d_name);
+				counts->dirs++;
+				count(subpath, counts);
+			}
+		} else {
+			counts->files++;
+		}
+	}
 
-    closedir(dir);
+	closedir(dir);
 }
