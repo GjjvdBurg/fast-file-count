3d945b1aa096b71866a8120a6c15ba62647c4230
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2018-03-24 15:50:09 +0000

    use windows builtin functions instead of dirent

--- c_count.c
+++ c_count.c
@@ -18,23 +18,25 @@
  *   - Added options for recurse, hidden, and quiet
  *   - Removed debug code
  *   - Reformatted long lines
+ *   - Use builtin windows instead of dirent
  *
  */
 #include <stdio.h>
-#include <dirent.h>
 #include <string.h>
 #include <stdlib.h>
 #include <limits.h>
-#include <sys/stat.h>
-#include <stdbool.h>
 
 #if defined(WIN32) || defined(_WIN32)
-#include <Windows.h>
 #define ON_WINDOWS
-#define PATH_SEP '\\'
+#include <windows.h>
+#include <tchar.h>
+#include <strsafe.h>
 #else
-#define PATH_SEP '/'
+#include <dirent.h>
+#include <sys/stat.h>
 #endif
+
+typedef enum { false, true } bool;
 
 /* A custom structure to hold separate file and directory counts */
 struct filecount {
@@ -43,13 +45,80 @@ struct filecount {
 };
 
 
+#ifdef ON_WINDOWS
+void count(TCHAR *path, struct filecount *counts, bool recursive, bool hidden, 
+		bool quiet)
+{
+	WIN32_FIND_DATA ffd;
+	HANDLE hFind = INVALID_HANDLE_VALUE;
+	DWORD dwError = 0;
+	TCHAR subpath[MAX_PATH];
+	size_t path_length;
+	size_t subpath_length;
+
+	hFind = FindFirstFile(path, &ffd);
+	if (hFind == INVALID_HANDLE_VALUE) {
+		if (!quiet)
+			fprintf(stderr, "FindFirstFile failed for %s "
+					"(error code %d)\n", path, 
+					GetLastError());
+		return;
+	}
+
+	do {
+		StringCchLength(path, MAX_PATH, &path_length);
+		StringCchLength(ffd.cFileName, MAX_PATH, &subpath_length);
+		subpath_length += path_length + 1;
+		if (subpath_length > MAX_PATH) {
+			if (!quiet)
+				fprintf(stderr, "path too long (%ld) %s%s%s",
+						subpath_length, path, 
+						TEXT("\\"), ffd.cFileName);
+			return;
+		}
+
+		if (!hidden) {
+			if (ffd.dwFileAttributes & FILE_ATTRIBUTE_HIDDEN)
+				continue;
+		}
+
+		if (ffd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+			if (lstrcmp(ffd.cFileName, TEXT(".")) == 0)
+				continue;
+			if (lstrcmp(ffd.cFileName, TEXT("..")) == 0)
+				continue;
+
+			counts->dirs++;
+			if (recursive) {
+				StringCchCopyN(subpath, MAX_PATH, path, 
+						path_length - 2);
+				StringCchCat(subpath, MAX_PATH, TEXT("\\"));
+				StringCchCat(subpath, MAX_PATH, ffd.cFileName);
+				StringCchCat(subpath, MAX_PATH, TEXT("\\*"));
+				count(subpath, counts, recursive, hidden,
+						quiet);
+			}
+		} else {
+			counts->files++;
+		}
+	} while (FindNextFile(hFind, &ffd) != 0);
+
+	dwError = GetLastError();
+	if (dwError != ERROR_NO_MORE_FILES) {
+		_tprintf(TEXT("FindFirstFile"));
+	}
+
+	FindClose(hFind);
+}
+
+#else
+
 bool str_startswith(const char *str, const char *pre)
 {
 	size_t lenpre = strlen(pre),
 	       lenstr = strlen(str);
 	return lenstr < lenpre ? false : strncmp(pre, str, lenpre) == 0;
 }
-
 
 /*
  * counts the number of files and directories in the specified directory.
@@ -88,26 +157,20 @@ void count(char *path, struct filecount *counts, bool recursive, bool hidden,
 		if (len > PATH_MAX) {
 			if (!quiet)
 				fprintf(stdout, "path too long (%ld) %s%c%s", 
-						len, path, PATH_SEP, ent->d_name);
+						len, path, '/', ent->d_name);
 			return;
 		}
 
 		if (!hidden) {
-			#ifdef ON_WINDOWS
-			sprintf(subpath, "%s%c%s", path, PATH_SEP, ent->d_name);
-			if (GetFileAttribute(_T(subpath)) & FILE_ATTRIBUTE_HIDDEN)
-				continue;
-			#else
 			if (str_startswith(ent->d_name, "."))
 				continue;
-			#endif
 		}
 
 		/* Use dirent.d_type if present, otherwise use stat() */
 		#if ( defined ( _DIRENT_HAVE_D_TYPE ))
 		if(DT_DIR == ent->d_type) {
 		#else
-		sprintf(subpath, "%s%c%s", path, PATH_SEP, ent->d_name);
+		sprintf(subpath, "%s%c%s", path, '/', ent->d_name);
 		if(lstat(subpath, &statbuf)) {
 			if (!quiet)
 				perror(subpath);
@@ -125,7 +188,7 @@ void count(char *path, struct filecount *counts, bool recursive, bool hidden,
 
 			// if we have d_type, we don't have subpath yet
 			#if ( defined ( _DIRENT_HAVE_D_TYPE ))
-			sprintf(subpath, "%s%c%s", path, PATH_SEP, ent->d_name);
+			sprintf(subpath, "%s%c%s", path, '/', ent->d_name);
 			#endif
 			counts->dirs++;
 			if (recursive)
@@ -136,20 +199,32 @@ void count(char *path, struct filecount *counts, bool recursive, bool hidden,
 	}
 	closedir(dir);
 }
-
+#endif
 
 void c_fast_file_count(char *path, long l_recursive, long l_hidden,
 		long l_quiet, long *result_files, long *result_dirs)
 {
-	bool recursive = l_recursive;
-	bool hidden = l_hidden;
-	bool quiet = l_quiet;
-
+	bool recursive, hidden, quiet;
 	struct filecount counts;
+
+	#ifdef ON_WINDOWS
+	TCHAR thePath[MAX_PATH];
+	StringCchCopy(thePath, MAX_PATH, path);
+	StringCchCat(thePath, MAX_PATH, TEXT("\\*"));
+	#endif
+
+	recursive = l_recursive;
+	hidden = l_hidden;
+	quiet = l_quiet;
+
 	counts.files = 0;
 	counts.dirs = 0;
 
+	#ifdef ON_WINDOWS
+	count(thePath, &counts, recursive, hidden, quiet);
+	#else
 	count(path, &counts, recursive, hidden, quiet);
+	#endif
 
 	*result_files = counts.files;
 	*result_dirs = counts.dirs;
